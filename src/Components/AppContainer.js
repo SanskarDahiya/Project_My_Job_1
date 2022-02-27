ddcd28fad76d41d9fe42ec663d5401b61e1dc42f
Sanskar Dahiya <sanskar.dahiya@daffodilsw.com>
2022-02-27 20:36:29 +0530

    update

--- src/Components/AppContainer.js
+++ src/Components/AppContainer.js
@@ -1,26 +1,29 @@
 import React from "react";
-
+import { version } from "./../../package.json";
 const AppContainer = () => {
   return (
-    <div
-      style={{
-        background: "#EDF6FF 0% 0% no-repeat padding-box",
-        position: "absolute",
-        zIndex: "-1",
-        height: "-webkit-fill-available",
-        width: "100%"
-      }}
-    >
+    <React.Fragment>
+      <div id="version-name">{version}</div>
       <div
         style={{
-          position: "relative",
-          height: "300px",
-          width: "100%",
-          background: "transparent linear-gradient(253deg, #303F60 0%, #1A253C 100%) 0% 0% no-repeat padding-box",
-          opacity: "1"
+          background: "#EDF6FF 0% 0% no-repeat padding-box",
+          position: "absolute",
+          zIndex: "-1",
+          height: "-webkit-fill-available",
+          width: "100%"
         }}
-      />
-    </div>
+      >
+        <div
+          style={{
+            position: "relative",
+            height: "300px",
+            width: "100%",
+            background: "transparent linear-gradient(253deg, #303F60 0%, #1A253C 100%) 0% 0% no-repeat padding-box",
+            opacity: "1"
+          }}
+        />
+      </div>
+    </React.Fragment>
   );
 };
 
